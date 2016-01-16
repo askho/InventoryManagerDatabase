e246141d5f887f77ec03f3d40319c690b50ade9f
Leon Ho <leon.lai.ho@gmail.com>
2016-01-16 00:06:48 -0800

    Added foreign key

--- InventoryDatabase/Inventory.sql
+++ InventoryDatabase/Inventory.sql
@@ -11,5 +11,6 @@
     [date_checked_out] DATETIME NULL, 
     CONSTRAINT [FK_Inventory_Company] FOREIGN KEY (company_id) REFERENCES [Company]([company_id]),
 	CONSTRAINT [FK_Inventory_Event] FOREIGN KEY (event_id) REFERENCES [Event]([event_id]),
-	CONSTRAINT [FK_Inventory_Member] FOREIGN KEY (member_id) REFERENCES [Member]([member_id])
+	CONSTRAINT [FK_Inventory_Member] FOREIGN KEY (member_id) REFERENCES [Member]([member_id]),
+	CONSTRAINT [FK_Inventory_Item] FOREIGN KEY (item_id) REFERENCES [Item]([item_id])
 )
